1c3e9bd5a5f3f3380ea521626107d7aef1103541
kirdreamer <3alog2@gmail.com>
2024-03-14 22:17:17 +0100

    Code Formatting fix

--- service/api/weather_api.py
+++ service/api/weather_api.py
@@ -11,6 +11,6 @@ propertiesManager = PropertiesManager()
 def get_weather_for_now(city: str = propertiesManager.read_property("default.city")):
     requestManager = RequestManager()
     arguments = {"q": city, "appid": propertiesManager.read_property("appid")}
-    response = requestManager.performGetRequest(
+    response = requestManager.perform_get_request(
         requestManager.add_arguments_to_url(propertiesManager.read_property("weather.url"), arguments))
     return f'Response Body: {response.data.decode("utf-8")}'
